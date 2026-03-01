71a6a3acbffb6f5e901e73bf71f0d5c665e4cf7a
emiresh <ireshek@gmail.com>
2026-03-01 21:46:16 +0530

    updated the sec list

--- terraform/security_list.tf
+++ terraform/security_list.tf
@@ -81,6 +81,54 @@ resource "oci_core_security_list" "default_sec_list" {
     }
   }
   ingress_security_rules {
+    description = "Kubelet API (control plane to workers)"
+    protocol    = "6"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+
+    tcp_options {
+      max = 10250
+      min = 10250
+    }
+  }
+  ingress_security_rules {
+    description = "etcd server client API"
+    protocol    = "6"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+
+    tcp_options {
+      max = 2380
+      min = 2379
+    }
+  }
+  ingress_security_rules {
+    description = "kube-scheduler and kube-controller-manager"
+    protocol    = "6"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+
+    tcp_options {
+      max = 10259
+      min = 10257
+    }
+  }
+  ingress_security_rules {
+    description = "NodePort Services"
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+
+    tcp_options {
+      max = 32767
+      min = 30000
+    }
+  }
+  ingress_security_rules {
     protocol    = "6"
     source      = "0.0.0.0/0"
     source_type = "CIDR_BLOCK"
